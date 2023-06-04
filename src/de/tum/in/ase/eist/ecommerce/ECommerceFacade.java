9c6de44d9c1649cb8b27b66948cccd8ad8d996fb
moritzslz <schultzmoritz@gmail.com>
2023-06-04 11:56:20 +0200

    3

--- src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
+++ src/de/tum/in/ase/eist/ecommerce/ECommerceFacade.java
@@ -2,34 +2,34 @@ package de.tum.in.ase.eist.ecommerce;
 
 public class ECommerceFacade {
 
-    private OrderController oc;
-    private AdvertisementController ac;
-    private ShippingController sc;
+    private OrderController orderController;
+    private AdvertisementController advertisementController;
+    private ShippingController shippingController;
     public ECommerceFacade(){
-        this.oc = new OrderController();
-        this.ac = new AdvertisementController();
-        this.sc = new ShippingController();
+        this.orderController =  new OrderController();
+        this.advertisementController = new AdvertisementController();
+        this.shippingController = new ShippingController();
     }
 
     public void processOrder(Order order){
-        oc.processOrder(order);
+        orderController.processOrder(order);
     }
 
     public void processOrder(Order order, String phoneNumber){
-        oc.processOrder(order, phoneNumber);
+        orderController.processOrder(order, phoneNumber);
     }
 
     public Order retrieveLatestOrder(int id){
-        return oc.retrieveLatestOrder(id);
+        return  orderController.retrieveLatestOrder(id);
     }
 
     public void playAdvertisement(int ageRestriction){
-        ac.playAdvertisement(ageRestriction);
+        advertisementController.playAdvertisement(ageRestriction);
     }
 
-    public void shipOrder(Order order, String adress){
-        order.setShipping(sc.createShipping(adress));
-        sc.shipOrder(order);
+    public void shipOrder(Order order, String address){
+        order.setShipping(shippingController.createShipping(address));
+        shippingController.shipOrder(order);
     }
 
 }
